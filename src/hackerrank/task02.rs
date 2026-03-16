7b0dbd54b96da2a6f065d2ea0ae0f21b6cb83b8d
Libery1 <ilya.sholudko.07@gmail.com>
2026-03-16 19:38:56 +0200

    .

--- src/hackerrank/task02.rs
+++ src/hackerrank/task02.rs
@@ -1,1 +1,42 @@
-// https://www.hackerrank.com/challenges/staircase/problem
+https://www.hackerrank.com/challenges/diagonal-difference/problem
+use std::io::{self, BufRead};
+
+
+fn diagonal_difference(arr: &[Vec<i32>]) -> i32 {
+    let n = arr.len();
+    let mut primary_sum = 0;
+    let mut secondary_sum = 0;
+
+    for i in 0..n {
+        primary_sum += arr[i][i];
+        
+        secondary_sum += arr[i][n - 1 - i];
+    }
+
+    (primary_sum - secondary_sum).abs()
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    
+    let n: usize = lines.next()
+        .unwrap().unwrap()
+        .trim()
+        .parse().unwrap();
+
+    let mut matrix: Vec<Vec<i32>> = Vec::new();
+
+    for _ in 0..n {
+        let row: Vec<i32> = lines.next()
+            .unwrap().unwrap()
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        matrix.push(row);
+    }
+
+    let result = diagonal_difference(&matrix);
+
+    println!("{}", result);
+}
