ecd6b966877e147e2ef56fac81ad35f1f3b69f4b
Libery1 <ilya.sholudko.07@gmail.com>
2026-03-16 19:34:27 +0200

    Update task01.rs

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,1 +1,29 @@
-// https://www.hackerrank.com/challenges/sock-merchant/problem
+https://www.hackerrank.com/challenges/birthday-cake-candles
+use std::io::{self, BufRead};
+
+fn birthday_cake_candles(candles: &[i32]) -> i32 {
+    let max_val = match candles.iter().max() {
+        Some(&max) => max,
+        None => return 0,
+    };
+
+    candles.iter().filter(|&&x| x == max_val).count() as i32
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut iterator = stdin.lock().lines();
+
+    let _n = iterator.next().unwrap().unwrap();
+
+    let candles_line = iterator.next().unwrap().unwrap();
+    
+    let candles: Vec<i32> = candles_line
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let result = birthday_cake_candles(&candles);
+
+    println!("{}", result);
+}
