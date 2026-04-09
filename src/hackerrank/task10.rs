897250407a92eca26ad65cdf452eff7393a0ef75
mavch <ilya.sholudko.07@gmail.com>
2026-04-10 02:42:52 +0300

    .

--- src/hackerrank/task10.rs
+++ src/hackerrank/task10.rs
@@ -1,1 +1,35 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
+use std::collections::HashMap;
+use std::io;
+
+fn sock_merchant(ar: Vec<i32>) -> i32 {
+    let mut map = HashMap::new();
+    let mut pairs = 0;
+
+    for sock in ar {
+        let count = map.entry(sock).or_insert(0);
+        *count += 1;
+
+        if *count % 2 == 0 {
+            pairs += 1;
+        }
+    }
+
+    pairs
+}
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+    input.clear();
+
+    io::stdin().read_line(&mut input).unwrap();
+    let ar: Vec<i32> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let result = sock_merchant(ar);
+    println!("{}", result);
+}
