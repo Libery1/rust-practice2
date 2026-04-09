e0baed90391a35c236b66ea0b485d244c559d092
mavch <ilya.sholudko.07@gmail.com>
2026-04-10 02:01:51 +0300

    .

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,27 @@
 // https://www.hackerrank.com/challenges/kangaroo/problem
+use std::io;
+
+fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> String {
+    if v1 <= v2 {
+        return "NO".to_string();
+    }
+
+    if (x2 - x1) % (v1 - v2) == 0 {
+        "YES".to_string()
+    } else {
+        "NO".to_string()
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let values: Vec<i32> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let result = kangaroo(values[0], values[1], values[2], values[3]);
+    println!("{}", result);
+}
